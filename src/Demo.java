cb437bcadf7bd4c3a50f4e9f47ee2d59f11f36cc
unknown <you@example.com>
2017-07-12 22:03:41 +0530

    hello

---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+class Demo {
+    public static void main(String a[]) {
+        System.out.println("A : "+a[0]+ " Is this value "+a[1]);
+        int number = 20;
+        System.out.println("Number : "+number);
+
+    }
+}
